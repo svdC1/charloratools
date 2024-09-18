0a0be39f471b51bfc344d59cdac3b84ebb259a1f
svdc <svdC1mail@gmail.com>
2024-09-18 19:58:03 -0300

    Implementing lazy loading of submodules so CLI script can run without torch being installed

--- src/charloratools/__init__.py
+++ src/charloratools/__init__.py
@@ -1,7 +1,18 @@
-from . import errors
-from . import FilterAI
-from . import Scrapers
-from . import SysFileManager
-from . import utils
-from . import facenet_pytorch
-from . import cli
+import importlib
+
+
+def __getattr__(name):
+    if name in ['cli', 'errors', 'FilterAI', 'Scrapers',
+                'SysFileManager', 'utils', 'facenet_pytorch']:
+        try:
+            return importlib.import_module(f'.{name}', __name__)
+        except ImportError as e:
+            if 'torch' in str(e):
+                raise ImportError(
+                    f"""Submodule '{name}' requires 'torch' but it is
+                        not installed.Please run 'charloratools install_torch'
+                        to install it."""
+                ) from None
+            else:
+                raise
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
